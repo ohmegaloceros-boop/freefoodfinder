da712236d22f08ae27cf4e2cd4f46e1336ea1373
Dallas Skare <ohmegaloceros@gmail.com>
2025-12-16 20:03:42 -0700

    Style: Change button text to lowercase 'submit new location'

--- client/src/App.js
+++ client/src/App.js
@@ -243,7 +243,7 @@ function App() {
             className="suggest-button"
             onClick={handleStartSelecting}
           >
-            {isSelectingOnMap ? '📍 Click on Map...' : 'Submit new location'}
+            {isSelectingOnMap ? '📍 Click on Map...' : 'submit new location'}
           </button>
         </div>
       </header>
